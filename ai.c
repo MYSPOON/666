b6272b3bf5b4f3adf8aa4eef2955edbd3453730a
MYSPOON <44917432+MYSPOON@users.noreply.github.com>
2018-12-11 15:30:55 +0800

    Add files via upload

--- ai.c
+++ ai.c
@@ -1,14 +1,25 @@
 #include "ai.h"
 
+
+
 // bool
+
 typedef int BOOL;
+
 #define TRUE 1
+
 #define FALSE 0
+
 char board[BOARD_SIZE][BOARD_SIZE] = { 0 };
+
 const int DIR[8][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1} };
 
+
+
 BOOL isInBound(int x, int y) {
+
 	return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
+
 }
 
 
@@ -16,39 +27,80 @@ BOOL isInBound(int x, int y) {
 
 
 
+
+
+
+
+
+
+
 /**
+
  * YOUR CODE BEGIN
+
  * 你的代码开始
+
  */
+
+
 
  /**
+
   * You can define your own struct and variable here
+
   * 你可以在这里定义你自己的结构体和变量
+
   */
+
 int myflag;
+
 int max_x, max_y, max_k;
 
+struct Command m_command = { 0,0,0 };
+
 /**
+
  * 你可以在这里初始化你的AI
+
  */
+
 void initAI(int me) {}
 
+
+
 int evaluate(int flag) {
+
 	int intervention_dir[4][2] = { {1, 0}, {0, 1}, {1, 1}, {1, -1} };
+
 	int custodian_dir[8][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+
 	int score = 0;
+
 	for (int k = 0; k < 8; k++) {
+
 		for (int i = 0; i < BOARD_SIZE; i++) {
+
 			for (int j = 0; j < BOARD_SIZE; j++) {
+
 				if (board[i][j] != flag) {
+
 					continue;
+
 				}
+
 				score += 100;
+
 			}
+
 		}
+
 	}
+
 	return score;
+
 }
+
+
 
 int alphabeta(int depth, int alpha, int beta, int flag) {
 	int other_flag = 3 - flag;
@@ -62,9 +114,9 @@ int alphabeta(int depth, int alpha, int beta, int flag) {
 			cur_board[i][j] = board[i][j];
 		}
 	}
-	if (myflag==flag) {
-		for (int di = 0; di< 8; di++) {
-			for (int x = 0; x< BOARD_SIZE; x++) {
+	if (myflag == flag) {
+		for (int di = 0; di < 8; di++) {
+			for (int x = 0; x < BOARD_SIZE; x++) {
 				for (int y = 0; y < BOARD_SIZE; y++) {
 					if (board[x][y] != flag) {
 						continue;
@@ -81,7 +133,7 @@ int alphabeta(int depth, int alpha, int beta, int flag) {
 							int y1 = new_y + intervention_dir[i][1];
 							int x2 = new_x - intervention_dir[i][0];
 							int y2 = new_y - intervention_dir[i][1];
-							if (isInBound(x1, y1) && isInBound(x2, y2) && board[x1][y1] == other_flag && board[x2][y2] == other_flag){
+							if (isInBound(x1, y1) && isInBound(x2, y2) && board[x1][y1] == other_flag && board[x2][y2] == other_flag) {
 								board[x1][y1] = flag;
 								board[x2][y2] = flag;
 							}
@@ -96,10 +148,7 @@ int alphabeta(int depth, int alpha, int beta, int flag) {
 								board[x1][y1] = flag;
 							}
 						}
-						
-
-						int now_score = alphabeta(depth-1,alpha,beta,other_flag);
-
+						int now_score = alphabeta(depth - 1, alpha, beta, other_flag);
 						//取消移动影响
 						for (int i = 0; i < BOARD_SIZE; i++) {
 							for (int j = 0; j < BOARD_SIZE; j++) {
@@ -109,6 +158,7 @@ int alphabeta(int depth, int alpha, int beta, int flag) {
 						if (now_score > alpha) {
 							alpha = now_score;
 						}
+
 						if (alpha > beta) {
 							return alpha;
 						}
@@ -119,67 +169,128 @@ int alphabeta(int depth, int alpha, int beta, int flag) {
 		return alpha;
 	}
 	else {
+
 		for (int di = 0; di < 8; di++) {
+
 			for (int x = 0; x < BOARD_SIZE; x++) {
+
 				for (int y = 0; y < BOARD_SIZE; y++) {
+
 					if (board[x][y] != other_flag) {
+
 						continue;
+
 					}
+
 					new_x = x + DIR[di][0];
+
 					new_y = y + DIR[di][1];
 
+
+
 					if (isInBound(new_x, new_y) && board[new_x][new_y] == EMPTY) {
+
 						//移动棋子
+
 						board[x][y] = EMPTY;
+
 						board[new_x][new_y] = other_flag;
+
 						int intervention_dir[4][2] = { {1, 0}, {0, 1}, {1, 1}, {1, -1} };
+
 						for (int i = 0; i < 4; i++) {
+
 							int x1 = new_x + intervention_dir[i][0];
+
 							int y1 = new_y + intervention_dir[i][1];
+
 							int x2 = new_x - intervention_dir[i][0];
+
 							int y2 = new_y - intervention_dir[i][1];
+
 							if (isInBound(x1, y1) && isInBound(x2, y2) && board[x1][y1] == flag && board[x2][y2] == flag) {
+
 								board[x1][y1] = other_flag;
+
 								board[x2][y2] = other_flag;
+
 							}
+
 						}
+
 						int custodian_dir[8][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+
 						for (int i = 0; i < 8; i++) {
+
 							int x1 = new_x + custodian_dir[i][0];
+
 							int y1 = new_y + custodian_dir[i][1];
+
 							int x2 = new_x + custodian_dir[i][0] * 2;
+
 							int y2 = new_y + custodian_dir[i][1] * 2;
+
 							if (isInBound(x1, y1) && isInBound(x2, y2) && board[x2][y2] == other_flag && board[x1][y1] == flag) {
+
 								board[x1][y1] = other_flag;
 
+
+
 							}
+
 						}
 
-						int now_score = alphabeta(depth -1, alpha, beta, other_flag);
+
+
+						int now_score = alphabeta(depth - 1, alpha, beta, other_flag);
+
+
 
 						//取消移动影响
+
 						for (int i = 0; i < BOARD_SIZE; i++) {
+
 							for (int j = 0; j < BOARD_SIZE; j++) {
+
 								board[i][j] = cur_board[i][j];
+
 							}
+
 						}
 
-						if (now_score <beta) {
+
+
+						if (now_score < beta) {
+
 							beta = now_score;
+
 						}
+
 						if (alpha >= beta) {
+
 							return beta;
+
 						}
+
 					}
+
 				}
+
 			}
+
 		}
+
 		return beta;
+
 	}
 
+
+
 }
-struct Command tomove(char board[BOARD_SIZE][BOARD_SIZE]) {
+
+struct Command tomove(char board[BOARD_SIZE][BOARD_SIZE],int flag) {
 	char now_board[BOARD_SIZE][BOARD_SIZE];
+	int other_flag = 3 - flag;
 	int m_score, cur_score;
 	if (myflag == 1) {
 		m_score = -10000;
@@ -187,35 +298,103 @@ struct Command tomove(char board[BOARD_SIZE][BOARD_SIZE]) {
 	else {
 		m_score = 10000;
 	}
-	for (int i = 0; i < BOARD_SIZE; i++) {
-		for (int j = 0; j < BOARD_SIZE; j++) {
-			if (board[i][j] != myflag)
-				continue;
-
+	for (int k = 0; k < BOARD_SIZE; k++) {
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++) {
+				if (board[i][j] != myflag) {
+					continue;
+				}
+				int new_i = i + DIR[k][0];
+				int new_j = j + DIR[k][1];
+				if (isInBound(new_i, new_j) && board[new_i][new_j] == EMPTY) {
+					int intervention_dir[4][2] = { {1, 0}, {0, 1}, {1, 1}, {1, -1} };
+					for (int i = 0; i < 4; i++) {
+						int x1 = new_i + intervention_dir[i][0];
+						int y1 = new_j + intervention_dir[i][1];
+						int x2 = new_i - intervention_dir[i][0];
+						int y2 = new_j - intervention_dir[i][1];
+						if (isInBound(x1, y1) && isInBound(x2, y2) && board[x1][y1] == other_flag && board[x2][y2] == other_flag) {
+							board[x1][y1] = flag;
+							board[x2][y2] = flag;
+						}
+					}
+					int custodian_dir[8][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+					for (int i = 0; i < 8; i++) {
+						int x1 = new_i + custodian_dir[i][0];
+						int y1 = new_j + custodian_dir[i][1];
+						int x2 = new_i + custodian_dir[i][0] * 2;
+						int y2 = new_j + custodian_dir[i][1] * 2;
+						if (isInBound(x1, y1) && isInBound(x2, y2) && board[x2][y2] == flag && board[x1][y1] == other_flag) {
+							board[x1][y1] = flag;
+						}
+					}
+					if (flag == 1) {
+						cur_score = alphabeta(4, -10000, 10000, other_flag);
+						if (cur_score < m_score) {
+							cur_score = m_score;
+						}
+						if ( cur_score!= m_score) {
+							m_command.x = i; m_command.y = j; m_command.option = k;
+						}
+					}
+					else {
+						cur_score = alphabeta(4, -10000, 10000, other_flag);
+						if (cur_score > m_score) {
+							cur_score = m_score;
+						}
+						if (cur_score != m_score) {
+							m_command.x = i; m_command.y = j; m_command.option = k;
+						}
+					}
+				}
+			}
 		}
 	}
-
+	return m_command;
 }
+
 /**
+
  * 轮到你落子。
+
  * 棋盘上0表示空白，1表示黑棋，2表示白旗
+
  * me表示你所代表的棋子(1或2)
+
  * 你需要返回一个结构体Command，在x属性和y属性填上你想要移动的棋子的位置，option填上你想要移动的方向。
+
  */
+
 struct Command aiTurn(const char board[BOARD_SIZE][BOARD_SIZE], int flag) {
+
 	struct Command preferedPos = { 0,0,0 };
+
 	myflag = flag;
+
 	int first = 1;
+
 	if (flag == 1 && first == 1) {
+
 		struct Command preferedPos = { 9,7,4 };
+
 		first++;
+
 	}
+
 	else {
-		struct Command preferedPos = tomove(board);
+
+		struct Command preferedPos = tomove(board,flag);
+
 	}
+
 	return preferedPos;
+
 }
 
+
+
 /**
+
  * 你的代码结束
+
  */
